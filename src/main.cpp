9a5a85602c7d2fe047f70147d60b1dcc4e5e0299
Boodskap <platform@boodskap.io>
2017-11-01 19:57:55 +0530

    removed unwated defines and includes

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,6 @@
 #include <BoodskapHttpTransceiver.h>
 #endif
 
-#if defined(IMPL_TOUCH_SENSOR)
-#pragma message("*** Using Touch Sensor Implementation ****")
-#include <TouchSensor.h>
-#elif defined(IMPL_TEMP_SENSOR)
-#pragma message("*** Using Temperature Sensor Implementation ****")
-#include <TemperatureSensor.h>
-#else
-#error "*** No Implementation Selected, Use one of IMPL_TOUCH_SENSOR, IMPL_TEMP_SENSOR, etc... ***"
-#endif
-
-
 void setup() {
 
   DEBUG_PORT.begin(BAUD_RATE);
